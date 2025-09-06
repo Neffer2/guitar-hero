579d137ed4489d431fa8da9ff18630a6331154cb
Neffer2 <n3f73r@gmail.com>
2025-09-06 18:54:04 -0500

    1. Touch funcional

--- src/scenes/game.js
+++ src/scenes/game.js
@@ -22,9 +22,14 @@ export class Game extends Phaser.Scene {
         { note: 13, seg: 8.844, pos: 1 },
         { note: 14, seg: 9.116, pos: 0 }
     ];
+    positions = [];
     notes = [];
     width;
     height;
+    leftFret;
+    middleFret;
+    rightFret
+    flame;
 
     constructor ()
     {
@@ -35,7 +40,88 @@ export class Game extends Phaser.Scene {
         this.add.image((this.width/2), (this.height/2), 'bg');
         this.add.image((this.width/2), (this.height/2), 'neck');
 
-        let positions = [((this.width/2) - 226), (this.width/2), ((this.width/2) + 226)];
+        this.leftFret = this.physics.add.sprite(((this.width/2) - 226), (this.height - 100), 'fret')
+                        .setImmovable(true)
+                        .setCircle(45, 55)
+                        .setInteractive();
+
+        this.middleFret = this.physics.add.sprite((this.width/2), (this.height - 100), 'fret')
+                        .setImmovable(true)
+                        .setCircle(45, 55)
+                        .setInteractive();
+
+        this.rightFret = this.physics.add.sprite(((this.width/2) + 226), (this.height - 100), 'fret')
+                        .setImmovable(true)
+                        .setCircle(45, 55)
+                        .setInteractive();
+
+        this.flame = this.add.sprite(((this.width/2) - 226), ((this.height/2) + 400), 'hit');    
+        this.flame.alpha = 0;
+
+
+        // Touhchpoints
+        this.leftFret.on('pointerdown', () => {
+            console.log('Left Fret');
+            mContext.notes.forEach(note => {
+                if (note.getBounds().contains(mContext.leftFret.x, mContext.leftFret.y)) {
+                    mContext.deleteNote(note);
+                    this.playFlame(0);
+                }
+            });
+        });
+
+        this.middleFret.on('pointerdown', () => {
+            console.log('Middle Fret');
+            mContext.notes.forEach(note => {
+                if (note.getBounds().contains(mContext.middleFret.x, mContext.middleFret.y)) {
+                    mContext.deleteNote(note);
+                    this.playFlame(1);
+                }
+            });
+        });
+
+        this.rightFret.on('pointerdown', () => {
+            console.log('Right Fret');
+            mContext.notes.forEach(note => {
+                if (note.getBounds().contains(mContext.rightFret.x, mContext.rightFret.y)) {
+                    mContext.deleteNote(note);
+                    this.playFlame(2);
+                }
+            });
+        });
+
+        // Scanner
+        this.input.keyboard.on('keydown', key => {
+            if (key.code === 'KeyA') {
+                console.log('A');                
+                mContext.notes.forEach(note => {
+                    if (note.getBounds().contains(mContext.leftFret.x, mContext.leftFret.y)) {
+                        mContext.deleteNote(note);
+                        this.playFlame(0);
+                    }
+                });
+            }
+
+            if (key.code === 'KeyS') {
+                console.log('S');
+                mContext.notes.forEach(note => {
+                    if (note.getBounds().contains(mContext.middleFret.x, mContext.middleFret.y)) {
+                        mContext.deleteNote(note);
+                        this.playFlame(1);
+                    }
+                });
+            }
+
+            if (key.code === 'KeyD') {
+                console.log('D');
+                mContext.notes.forEach(note => {
+                    if (note.getBounds().contains(mContext.rightFret.x, mContext.rightFret.y)) {
+                        mContext.deleteNote(note);
+                        this.playFlame(2);
+                    }
+                });
+            }
+        })
 
         window.videoState = function(event) {
             console.log('Video started');
@@ -46,8 +132,8 @@ export class Game extends Phaser.Scene {
              * -(((elem.seg + mContext.initAvg) * mContext.velocity) - mContext.tabLength)
             */
             mContext.tab.forEach((elem) => {
-                let note = mContext.physics.add.image(
-                    (positions[elem.pos]),  // X
+                let note = mContext.physics.add.sprite(
+                    (mContext.positions[elem.pos]),  // X
                     (-(((elem.seg + mContext.initAvg) * mContext.velocity) - mContext.tabLength)), // Y
                     'notes');
                 note.setVelocityY(mContext.velocity);
@@ -67,10 +153,34 @@ export class Game extends Phaser.Scene {
     init(){     
         mContext = this; 
         this.height = this.sys.game.config.height;
-        this.width = this.sys.game.config.width;                
+        this.width = this.sys.game.config.width;
+        this.positions = [((this.width/2) - 226), (this.width/2), ((this.width/2) + 226)];
+        
+        // Animations
+        this.anims.create({
+            key: 'hit',
+            frames: this.anims.generateFrameNumbers('hit', { start: 0, end: 5 }),
+            frameRate: 25,
+            repeat: 0
+        });
     }
 
     getRandomNumber(min, max){
         return Math.floor(Math.random() * (max - min)) + min;
+    }
+
+    deleteNote(note){
+        mContext.notes.splice(mContext.notes.indexOf(note), 1);
+        note.destroy();
+    } 
+    
+    playFlame(posX){
+        this.flame.x = this.positions[posX];
+        this.flame.alpha = 1;
+        this.flame.anims.play('hit', false);
+
+        this.flame.on('animationcomplete', () => {
+            this.flame.alpha = 0;
+        });
     }
 }
