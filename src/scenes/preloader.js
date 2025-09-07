07bda5d24c6f55ef94eaff3b7e3d1da1621ed89b
Neffer2 <n3f73r@gmail.com>
2025-09-06 19:03:06 -0500

    1. Tecla on temp

--- src/scenes/preloader.js
+++ src/scenes/preloader.js
@@ -9,7 +9,7 @@ export class Preloader extends Phaser.Scene {
         this.load.setPath('public/assets');
         this.load.spritesheet('notes', '/notes/notes.png', { frameWidth: 199, frameHeight: 103 });
         this.load.spritesheet('hit', '/notes/hit.png', { frameWidth: 252, frameHeight: 341 });
-        this.load.image('fret', '/notes/tecla_on.png');
+        this.load.image('fret', '/notes/tecla_on_temp.png');
         this.load.image('neck', '/neck/neck.png');
         this.load.image('bg', '/bg.png');
 
